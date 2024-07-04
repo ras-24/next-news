f15040725e8eac5a32a9b1125666fc87914d158c
Ras <siregar.riyando@gmail.com>
2024-07-04 10:46:24 +0800

    Server vs Client Component on main-header

--- app/archive/layout.js
+++ app/archive/layout.js
@@ -1,7 +1,7 @@
 export default function ArchiveLayout({ archive, latest }) {
   return (
     <div>
-      <h1>New Archive</h1>
+      <h1>News Archive</h1>
       <section id="archive-filter">{archive}</section>
       <section id="archive-latest">{latest}</section>
     </div>
